c91600f99b5f8054b56c944fbb99389ed22efbcd
ishaansk <4ishaan03@gmail.com>
2026-01-28 16:34:30 +0530

    Optimize requirements for Vercel: removed unused scikit-learn and matplotlib

--- api/index.py
+++ api/index.py
@@ -4,16 +4,16 @@ from statsbombpy import sb
 import pandas as pd
 import os
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__, static_folder='../', static_url_path='/')
 CORS(app)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/<path:path>')
 def send_static(path):
-    return send_from_directory('.', path)
+    return send_from_directory(app.static_folder, path)
 
 @app.route('/api/competitions', methods=['GET'])
 def get_competitions():
